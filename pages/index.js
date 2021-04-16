f8c3ebf5dc000904e349b4df709690fa92ab11b2
Sebastianus Sembara <sembara9090@gmail.com>
2021-04-17 03:08:49 +0700

    Clean Up

--- pages/index.js
+++ pages/index.js
@@ -20,38 +20,8 @@ export default function Home() {
         </p>
 
         <p className={styles.description}>
-        Get in touch via email at <a href="mailto:sembara9090@gmail.com">sembara9090@gmail.com</a> 
+          Get in touch via email at <a href="mailto:sembara9090@gmail.com">sembara9090@gmail.com</a>
         </p>
-
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
 
       <footer className={styles.footer}>
