83bdbf19e50a3f7cff6c1c40425f8f68180caf8d
Sebastianus Sembara <sembara9090@gmail.com>
2022-02-19 13:08:40 +0700

    feat: use typescript

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import NavBar from '../components/NavBar'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <NavBar/>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import { Box, Container } from '@chakra-ui/react'
+import Hero from '../components/Hero'
+
+const Home: NextPage = () => {
+  return (
+   <Hero/>
+  )
+}
+
+export default Home
