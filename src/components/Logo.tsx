583c78433df710dd9b92be7d216dc4c896c57f80
Sebastianus Sembara <sembara9090@gmail.com>
2024-05-04 15:46:55 +0800

    fix: update next link with new update next js

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -24,20 +24,18 @@ const Logo = () => {
 
   return (
     <Link href="/" scroll={false}>
-      <a>
-        <LogoBox>
-          <Image src={letterImage} width={20} height={20} alt="logo" />
-          &nbsp;
-          <Text
-            color={useColorModeValue("gray.800", "whiteAlpha.900")}
-            fontFamily='M PLUS Rounded 1c", sans-serif'
-            fontWeight="bold"
-            ml={3}
-          >
-            Sebastianus Sembara
-          </Text>
-        </LogoBox>
-      </a>
+      <LogoBox>
+        <Image src={letterImage} width={20} height={20} alt="logo" />
+        &nbsp;
+        <Text
+          color={useColorModeValue("gray.800", "whiteAlpha.900")}
+          fontFamily='M PLUS Rounded 1c", sans-serif'
+          fontWeight="bold"
+          ml={3}
+        >
+          Sebastianus Sembara
+        </Text>
+      </LogoBox>
     </Link>
   );
 };
